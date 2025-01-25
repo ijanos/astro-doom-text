70cec333704f915d8fe4ba135cad70977f35a543
János Illés <ijanos@gmail.com>
2025-01-25 09:54:13 +0100

    update demo page
    generate more combinations

--- packages/astro-doom-text-component/index.ts
+++ packages/astro-doom-text-component/index.ts
@@ -2,5 +2,7 @@
 // Then, use this file to export everything you want your user to access.
 
 import DoomText from './src/DoomText.astro';
+import type { Fonts } from './src/fonts';
 
 export { DoomText };
+export type { Fonts };
