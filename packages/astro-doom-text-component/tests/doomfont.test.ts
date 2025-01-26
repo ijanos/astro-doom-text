c0d70f989a6385cff27687362c45b1fd9b680532
János Illés <ijanos@gmail.com>
2025-01-26 18:39:12 +0100

    drop exact base64 string compare for now

--- packages/astro-doom-text-component/tests/doomfont.test.ts
+++ packages/astro-doom-text-component/tests/doomfont.test.ts
@@ -24,5 +24,4 @@ test('DoomText component creates a valid HTMLImageElement', async () => {
   expect(imgElement?.alt).toContain(text);
   expect(imgElement?.title).toContain(text);
   expect(imgElement?.src).toContain("data:image/png;base64,");
-  expect(imgElement?.src).toContain("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAALIAAAAOCAYAAACLgkZmAAAABmJLR0QA/wD/AP+gvaeTAAACL0lEQVRoge2aL1NCQRTFLw6RD0AwGowEooEZC4FoIBqMBKPBaDAajHwAg9FgccZgNBAJRAMfgI7B+xvgwGN57IrOmz3lvH2798++Zc8+7rzaudncIvBmVltux/oL+VeE4oXsf9tfqnhl80o9PnV+irLx1d9RGeOMjP+KOhf3zhPnoQy8cj5zvpR+dlTX223nhvCH85PYE79Z4D+EWHvyZ37X0v+wuJybpVfmR2kPJC8dx/P7sFXEzkPXuWGboX7AvutAnJ5zV+7zu3yXuOSfFTmjEqiHh/yAHTaV+6rEPel/cX4N+B85n+yaUCJ7VbC+s55InXXTpMqM4jTlPgqMQs+cj2Vc7DzAUBjgF3uND/ZdB+b1JAxaEn8Jc7OsyBkVwZois5NuCwxUMQA7lHcadljHmXfmT+eQQh8a5M+Jo/m1ZdxvgeeHAs6kHULsPDgBitYZv3fOu1YtdoX+7oiD0veln/llRc6oBNYUOVS1KHr3GTs3hd+dyypwd3G5104va//ljPJ1pX8q41ID/zzftvTTntl2xM6DftbvwhklJL+lE2Lr8y27DpxIeiK0pD2WdlbkjEpg56rFTBgs/Wufm5mdeoMdhZJ0nIsUXxWiJ6wYSHtfe80fUH8lX6ov1G1T15GB1m1vnHl3nNhmxM5D33FHtgqqCORR9I4du46KZ2dOCq2C5TpyRqVQ++tvLWL/9aayB4f+1iIUNzS/onwO9a2F+kv9rU0oHsiKnFEJfAPBW6DHAP12NgAAAABJRU5ErkJggg==");
 });
